9ced253dfeebc8a52c0bf092f8bf6e00282af9d2
Ganesamoorthi D <dganesamoorthi@temenos.com>
2020-01-09 18:31:41 +0530

    MSF-1047

--- msfwk-testsql/ms-payments-core/src/main/java/com/temenos/microservice/payments/function/PaymentOrderFunctionHelper.java
+++ msfwk-testsql/ms-payments-core/src/main/java/com/temenos/microservice/payments/function/PaymentOrderFunctionHelper.java
@@ -9,6 +9,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.temenos.microservice.framework.core.FunctionException;
 import com.temenos.microservice.framework.core.adapter.ServiceAdapterFactory;
 import com.temenos.microservice.framework.core.conf.Environment;
 import com.temenos.microservice.framework.core.function.Context;
@@ -28,7 +29,7 @@ public class PaymentOrderFunctionHelper {
 		}
 	}
 
-	public static void validatePaymentOrder(PaymentOrder paymentOrder, Context ctx) throws InvalidInputException {
+	public static void validatePaymentOrder(PaymentOrder paymentOrder, Context ctx) throws FunctionException {
 		List<FailureMessage> failureMessages = new ArrayList<FailureMessage>();
 		if (paymentOrder.getAmount() == null) {
 			failureMessages.add(new FailureMessage("Amount is mandatory", "PAYM-PORD-A-2101"));
@@ -51,11 +52,21 @@ public class PaymentOrderFunctionHelper {
 				}
 			} else {
 				HttpClientRequest request = new HttpClientRequest.Builder()
-						.basePath(Environment.getEnvironmentVariable("IRIS_BASE_URI", "http://10.93.27.98:9089/irf-provider-container/api/"))
+						.basePath(Environment.getEnvironmentVariable("IRIS_BASE_URI", "http://10.92.17.106:9089/irf-provider-container/api/"))
 						.resourcePath("v2.0.0/holdings/PSD2/accounts/" + paymentOrder.getFromAccount() + "/balances")
 						.context(ctx).build();
 
 				Response<String> response = ServiceAdapterFactory.getServiceAdapter().get(request);
+				if(response.getStatus() != 200 ) {
+					FunctionException exception = new FunctionException(response.getFailureMessages()) {
+						private static final long serialVersionUID = 1L;
+						@Override
+						public int getStatusCode() {
+							return response.getStatus();
+						}
+					};
+					throw exception;
+				}
 				irisApiResponse = response.getBody();
 			}
 
