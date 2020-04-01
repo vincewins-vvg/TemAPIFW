ea1dff9a2994ff64e410df259d51864c13ce5947
Ganesamoorthi D <dganesamoorthi@temenos.com>
2020-04-01 18:33:21 +0530

    business condition view change

--- msfwk-testnosql/ms-paymentorder-core/src/main/java/com/temenos/microservice/paymentorder/function/BusinessConditionImpl.java
+++ msfwk-testnosql/ms-paymentorder-core/src/main/java/com/temenos/microservice/paymentorder/function/BusinessConditionImpl.java
@@ -4,19 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.temenos.microservice.framework.core.health.BusinessHealthIndicator;
+import com.temenos.microservice.framework.core.health.view.BusinessHealth;
 import com.temenos.microservice.framework.core.health.view.BusinessHealthCheck;
 import com.temenos.microservice.framework.core.health.view.Status;
 
 
 public class BusinessConditionImpl implements BusinessHealthIndicator {
 	@Override
-	public BusinessHealthCheck doHealthCheck() {
-		BusinessHealthCheck businessHealthCheck = new BusinessHealthCheck();
-		businessHealthCheck.setCheckName("BusinessCondition");
-		businessHealthCheck.setStatus(Status.SUCCESS);
+	public BusinessHealth doHealthCheck() {
+		
+		BusinessHealth businessHealthCheck = new BusinessHealthCheck();
+		((BusinessHealthCheck) businessHealthCheck).setCheckName("BusinessCondition");
+		((BusinessHealthCheck) businessHealthCheck).setStatus(Status.SUCCESS);
 		Map<String,String> businessInfo = new HashMap<String,String>();
 		businessInfo.put("BusinessInfo", "Default Business Info");
-		businessHealthCheck.setMessage(businessInfo);
+		((BusinessHealthCheck) businessHealthCheck).setMessage(businessInfo);
 		return businessHealthCheck;
 	}
 }
