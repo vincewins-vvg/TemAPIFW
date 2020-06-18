7179f75d1c96475e5def8316b2872ecc9ad191e6
Vinod Shunmugavel <vinods@temenos.com>
2020-06-18 15:37:58 +0530

    MSF-1795: Migrate paymentorder from cassandra to mongo

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/paymentordercassandra.bat
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/paymentordercassandra.bat
@@ -14,5 +14,5 @@ REM Copy the environment file for docker to resolve
 copy %DOCKER_ENV_LOCATION%\ENV.env .env > NUL
 
 REM Now run Docker Compose
-docker-compose -f kafka.yml -f paymentorderMongo.yml %*
+docker-compose -f kafka.yml -f paymentordercassandra.yml %*
 	
