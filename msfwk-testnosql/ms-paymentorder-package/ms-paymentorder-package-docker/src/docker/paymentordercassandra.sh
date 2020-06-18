9ed08d52cdc6e6c2bff4ef597dc084010c131cea
Vinod Shunmugavel <vinods@temenos.com>
2020-06-18 15:37:58 +0530

    MSF-1795: Migrate paymentorder from cassandra to mongo

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/paymentordercassandra.sh
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/paymentordercassandra.sh
@@ -4,6 +4,13 @@
 # --------------------------------------------------------------
 
 if [ -z "$DOCKER_ENV_LOCATION" ]; then export DOCKER_ENV_LOCATION=config ;fi
+<<<<<<< HEAD
+=======
+if [ -z "$INGESTER_ENV_LOCATION" ]; then export INGESTER_ENV_LOCATION=app/ingester/ ;fi
+
+# Copy the local log4j2.properties to the ingester jar
+jar uf $INGESTER_ENV_LOCATION/ms-paymentorder-ingester.jar -C $INGESTER_ENV_LOCATION log4j2.properties
+>>>>>>> MSF-1795: Migrate paymentorder from cassandra to mongo
 
 # Copy the environment file for docker to resolve
 cp -f ${DOCKER_ENV_LOCATION}/ENV.env .env
