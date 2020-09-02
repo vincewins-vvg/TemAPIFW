e330b785eba0a5b6146414ddbcbe0908258d97fc
Logashwari Subramaniyan <s.logashwari@temenos.com>
2020-09-02 20:04:42 +0530

    MSF-2484

---
+++ msfwk-testnosql/ms-paymentorder-core/src/main/java/com/temenos/microservice/paymentorder/function/GetInputValidationImpl.java
@@ -0,0 +1,27 @@
+package com.temenos.microservice.paymentorder.function;
+
+import java.util.List;
+
+import com.temenos.microservice.framework.core.FunctionException;
+import com.temenos.microservice.framework.core.function.Context;
+import com.temenos.microservice.framework.core.function.FailureMessage;
+import com.temenos.microservice.framework.core.function.InvalidInputException;
+import com.temenos.microservice.paymentorder.view.GetPaymentDetails;
+
+public class GetInputValidationImpl implements GetInputValidation {
+
+	@Override
+	public GetPaymentDetails invoke(Context ctx, GetInputValidationInput input) throws FunctionException {
+		GetPaymentDetails paymentDetails = new GetPaymentDetails();
+		String paymentId = input.getParams().get().getPaymentId().get(0);
+		
+		List<String> errorList= paymentDetails.doValidate();
+		if(errorList.size()>0)
+			throw new InvalidInputException(new FailureMessage(errorList.toString()));
+		
+		paymentDetails.setPaymentId(paymentId);
+		
+		return paymentDetails;
+	}
+
+}
