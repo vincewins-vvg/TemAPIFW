8bdfd5b1fb0a5fb2b7e7111c64d609ef24895246
v.nitheshrayudu <v.nitheshrayudu@10.93.201.44>
2020-09-11 09:46:48 +0530

    MSF-2453:MS package for Dev Testing on Dynamodb in PaymentOrder

---
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/paymentorderDynamo.bat
@@ -0,0 +1,17 @@
+@echo off
+REM --------------------------------------------------------------
+REM - Script to start Service
+REM --------------------------------------------------------------
+
+if not defined DOCKER_ENV_LOCATION set DOCKER_ENV_LOCATION=config
+if not defined INGESTER_ENV_LOCATION set INGESTER_ENV_LOCATION=app/ingester/
+
+REM Copy the local log4j2.properties to the ingester jar
+jar uf %INGESTER_ENV_LOCATION%/ms-paymentorder-ingester.jar -C %INGESTER_ENV_LOCATION% log4j2.properties
+
+
+REM Copy the environment file for docker to resolve
+copy %DOCKER_ENV_LOCATION%\ENV.env .env > NUL
+
+REM Now run Docker Compose
+docker-compose -f kafka.yml -f paymentorderDynamo.yml %*
