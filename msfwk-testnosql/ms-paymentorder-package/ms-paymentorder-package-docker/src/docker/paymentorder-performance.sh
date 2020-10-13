6be707108d530f9d6e4960289ee3a37f38239973
Vincent Vijay <gvincentvijay@temenos.com>
2020-10-13 12:59:00 +0530

    Added pre and post test scripts for downloading jars and setting up env for runnning Performance Profiler tool

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/paymentorder-performance.sh
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/paymentorder-performance.sh
@@ -1,21 +1,7 @@
 #!/bin/bash -e
 # --------------------------------------------------------------
-# - Script to start Traceability Service
+# - Script to start Payment Order Service for checking its Performance
 # --------------------------------------------------------------
-
-export JOB_HOME=`pwd`
-export LOGS_DIR=$JOB_HOME/logs
-export PERF_LOGS_DIR=$JOB_HOME/perflogs
-export AGGREGATORJAR_DIR=$JOB_HOME/aggregator
-
-
-mvn dependency:get -Dartifact=com.temenos.profiler:bytecodecount-logger:1.0.0-SNAPSHOT:jar -U
-mvn dependency:get -Dartifact=com.temenos.profiler:bytecodecount-agent:1.0.0-SNAPSHOT:jar -U
-mvn dependency:copy -Dartifact=com.temenos.profiler:bytecodecount-logger:1.0.0-SNAPSHOT:jar -DoutputDirectory=$JOB_HOME/app/api -Dmdep.stripVersion=true -U
-mvn dependency:copy -Dartifact=com.temenos.profiler:bytecodecount-agent:1.0.0-SNAPSHOT:jar -DoutputDirectory=$JOB_HOME/app/api -Dmdep.stripVersion=true -U
-
-mvn dependency:get -Dartifact=com.temenos.profiler:bytecodecount-data-aggregator:1.0.0-SNAPSHOT:jar -U
-mvn dependency:copy -Dartifact=com.temenos.profiler:bytecodecount-data-aggregator:1.0.0-SNAPSHOT:jar -U -DoutputDirectory=$AGGREGATORJAR_DIR -Dmdep.stripVersion=true -U
 
 if [ -z "$DOCKER_ENV_LOCATION" ]; then export DOCKER_ENV_LOCATION=config ;fi
 
---
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/performance-posttestscript.sh
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------
+# - Script for running the necessary jars associated with the Performance Profiling Tool
+#Note: This script should be run only after running the tests associated with Performance
+# --------------------------------------------------------------
+export PO_HOME=`pwd`
+export AGGREGATOR_DIR=$PO_HOME/aggregator
+export PERFlOGS_DIR=$PO_HOME/perflogs
+
+echo 'Moving API performance logs from docker env to perflogs folder in local'
+docker cp ms-paymentorder-docker_api_1:usr/local/tomcat/logs/ $PERFlOGS_DIR
+
+#Run the aggregator jar
+java -jar $AGGREGATOR_DIR/bytecodecount-data-aggregator.jar
