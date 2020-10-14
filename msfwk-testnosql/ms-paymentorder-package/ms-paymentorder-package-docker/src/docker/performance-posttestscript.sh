834c79057e33667018e98b90af50a9415db4a69c
Vincent Vijay <gvincentvijay@temenos.com>
2020-10-14 15:09:26 +0530

    Changes to post test script

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/performance-posttestscript.sh
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/performance-posttestscript.sh
@@ -9,5 +9,9 @@ export PERFlOGS_DIR=$PO_HOME/perflogs
 echo 'Moving API performance logs from docker env to perflogs folder in local'
 docker cp ms-paymentorder-docker_api_1:usr/local/tomcat/logs/ $PERFlOGS_DIR
 
+#Delete the logs which are not required
+cd $PERFlOGS_DIR/logs
+rm -r `ls | grep -v "^[0-9]\_"`
+
 #Run the aggregator jar
 java -jar $AGGREGATOR_DIR/bytecodecount-data-aggregator.jar
