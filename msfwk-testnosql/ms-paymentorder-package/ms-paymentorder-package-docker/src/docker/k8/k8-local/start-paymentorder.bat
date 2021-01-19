dcecc8ef45cfe894b161c5ada6e4768e37df83f2
Abhinaya Ravichandran <r.abhinaya@temenos.com>
2021-01-19 12:14:29 +0530

    Package changes k8

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/k8-local/start-paymentorder.bat
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/k8-local/start-paymentorder.bat
@@ -4,27 +4,16 @@ REM - Script to start Paymentorder Service
 REM --------------------------------------------------------------
 
 REM - Build paymentorder images
+
+cd ../..
+
 call build.bat build
+
+cd k8/k8-local
 
 REM - Start knative services
 
-
-cd kubectl/100_db/mongo
-kubectl apply -f 100_create-mongo-ns.yaml
-cd crd
-kubectl apply -f 110_mongo_crd.yaml
-cd ../operator
-kubectl apply -f 120_operator.yaml
-kubectl apply -f 130_rb.yaml
-kubectl apply -f 140_role.yaml
-kubectl apply -f 150_sa.yaml
-cd ../rs
-kubectl apply -f 160_rs.yaml
-kubectl apply -f 170_mongo_services.yaml
-
-timeout /t 60 >nul
-
-cd ../../../110_svc
+cd kubectl/110_svc
 REM cd kubectl/110_svc
 
 kubectl apply -f 000_paymentorder-ns.yaml
@@ -46,13 +35,6 @@ kubectl apply -f schema-registry.yaml
 
 cd ../130_scheduler
 kubectl apply -f 100_scheduler-job.yaml
-timeout /t 30 >nul
 
 cd ../../
 
-timeout /t 90 >nul
-
-cd kubectl/100_db/mongo/rs
-kubectl apply -f mongo-setup.yaml
-
-cd ../../../..
--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/k8-local/stop-paymentorder.bat
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/k8-local/stop-paymentorder.bat
@@ -17,18 +17,6 @@ kubectl delete -f 100_paymentorder-api.yaml
 kubectl delete -f 001_paymentorder-configmap.yaml
 kubectl delete -f 000_paymentorder-ns.yaml
 
-cd ../100_db/mongo/rs
-kubectl delete -f mongo-setup.yaml
-kubectl delete -f 170_mongo_services.yaml
-kubectl delete -f 160_rs.yaml
-cd ../operator
-kubectl delete -f 150_sa.yaml
-kubectl delete -f 140_role.yaml
-kubectl delete -f 130_rb.yaml
-kubectl delete -f 120_operator.yaml
-cd ../crd
-kubectl delete -f 110_mongo_crd.yaml
-cd ../
-kubectl delete -f 100_create-mongo-ns.yaml
 
-cd ../../../
+
+cd ../../
