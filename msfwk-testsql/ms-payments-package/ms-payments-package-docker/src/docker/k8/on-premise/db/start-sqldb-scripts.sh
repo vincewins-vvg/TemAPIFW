139e64f91995b846905af53ee645360c6d784a44
Sadeesh Selvaraj <sadeesh.selvaraj@temenos.com>
2021-02-03 18:13:54 +0530

    MSF-3252 shell scripts added for POSQL

---
+++ msfwk-testsql/ms-payments-package/ms-payments-package-docker/src/docker/k8/on-premise/db/start-sqldb-scripts.sh
@@ -0,0 +1,6 @@
+
+kubectl apply -f namespace.yaml
+
+kubectl apply -f mysql-db.yaml
+
+kubectl apply -f db-secrets.yaml
---
+++ msfwk-testsql/ms-payments-package/ms-payments-package-docker/src/docker/k8/on-premise/start_payments.sh
@@ -0,0 +1,17 @@
+# --------------------------------------------------------------
+# - Script to start Service
+# --------------------------------------------------------------
+
+cd ../..
+
+./build.sh build
+
+cd k8/on-premise/db
+
+call start-sqldb-scripts.sh
+
+cd ../
+
+helm install svc ./svc
+
+# docker-compose -f kafka.yml -f paymentorder-nuo.yml %*
