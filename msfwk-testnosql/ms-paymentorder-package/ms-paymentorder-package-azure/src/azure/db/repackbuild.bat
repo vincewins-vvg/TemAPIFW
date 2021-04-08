03f7d58eff3ac2de754bbcd4426b1a0a28943cde
m.srimuthurajesh <m.srimuthurajesh@temenos.com>
2021-04-08 12:26:43 +0530

    MSF-3528 Azure repackbuild script changes for entity jar renaming

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-azure/src/azure/db/repackbuild.bat
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-azure/src/azure/db/repackbuild.bat
@@ -1,6 +1,6 @@
 @echo on
 
 for /D %%s in (.\target\azure-functions\*) do (
-    if exist target\azure-functions\%%~ns\lib\ms-entity.jar del /f target\azure-functions\%%~ns\lib\ms-entity.jar
-    xcopy db\dbjars\%1-entity-%2.jar target\azure-functions\%%~ns\lib\ms-entity.jar* /d
+    if exist target\azure-functions\%%~ns\lib\*-entity.jar del /f target\azure-functions\%%~ns\lib\*-entity.jar
+    xcopy db\dbjars\%1-entity-%2.jar target\azure-functions\%%~ns\lib\%1-entity.jar* /d
 )
