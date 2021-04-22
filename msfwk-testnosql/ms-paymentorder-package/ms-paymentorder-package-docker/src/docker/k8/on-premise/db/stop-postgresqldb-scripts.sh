edf7315f729a88410fb0efaf89aa27b4b02f0c29
m.srimuthurajesh <m.srimuthurajesh@temenos.com>
2021-04-22 14:56:31 +0530

    MSF-3664 Multiple entity seperation as default implementation for PO

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/on-premise/db/stop-postgresqldb-scripts.sh
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/on-premise/db/stop-postgresqldb-scripts.sh
@@ -2,8 +2,8 @@ cd database/postgresql
 
 kubectl delete -f postgresql-db.yaml
 
-kubectl delete -f namespace.yaml
-
 kubectl delete -f db-secrets.yaml
+
+kubectl delete -f namespace.yaml
 
 cd ../..
--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/on-premise/start-paymentorder-Postgresql.sh
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/on-premise/start-paymentorder-Postgresql.sh
@@ -11,14 +11,12 @@ export MONGODB_CONNECTIONSTR=mongodb+srv://badri0307:badri0307@mongodb01.qjebf.a
 
 cd ../..
 
-./build.sh create --build
+./build-Postgresql.sh create --build
 
 cd k8/on-premise/db
 
 ./start-postgresqldb-scripts.sh
 
 cd ../
-
-cd k8/on-premise/
 
 helm install ponosql ./svc --set env.database.MONGODB_CONNECTIONSTR=$MONGODB_CONNECTIONSTR --set env.database.MONGODB_DBNAME=$DB_NAME --set env.database.POSTGRESQL_CONNECTIONURL=jdbc:postgresql://po-postgresqldb-service.postgresql.svc.cluster.local:5432/paymentorderdb --set env.database.DATABASE_KEY=postgresql
