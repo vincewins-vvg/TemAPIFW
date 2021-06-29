666b811bdb2643fa508ca2bddc3fa94fcc35c84a
Sadeesh Selvaraj <sadeesh.selvaraj@temenos.com>
2021-06-29 18:06:32 +0530

    MFW-612 Helm pack enabled in PONOSQL

---
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/packMongoK8Zip.bat
@@ -0,0 +1,71 @@
+@echo off
+REM --------------------------------------------------------------
+REM - Script to create Entitlement Helm Pack
+REM --------------------------------------------------------------
+
+cd ../../target
+
+jar xf ms-paymentorder-package-docker-*.zip
+
+cd ms-paymentorder-Docker
+
+if not defined DOCKER_ENV_LOCATION set DOCKER_ENV_LOCATION=config
+set DATABASE=mongo
+set MSF_NAME=ms-paymentorder
+
+REM Copy the environment file for docker to resolve
+copy %DOCKER_ENV_LOCATION%\k8ENV.env .env > NUL
+REM copy %DOCKER_ENV_LOCATION%\k8ENV.env .env > NUL
+
+call repackbuild.bat app\api ms-paymentorder-api.war %* 
+call repackbuild.bat app\ingester ms-paymentorder-ingester.jar %* 
+call repackbuild.bat app\inboxoutbox ms-paymentorder-inboxoutbox.jar %* 
+call repackbuild.bat app\ms-framework-scheduler ms-paymentorder-scheduler.jar %* 
+
+REM Now run Docker Compose
+docker-compose -f db-build.yml %*
+
+REM Now run Docker Compose
+docker-compose -f paymentordermongo.yml %*
+
+cd paymentorder
+
+mkdir helm-chart
+
+cd helm-chart
+
+mkdir svc
+
+mkdir dbinit
+
+cd ../../
+
+xcopy k8\on-premise\svc paymentorder\helm-chart\svc /s /e
+
+xcopy k8\on-premise\dbinit paymentorder\helm-chart\dbinit /s /e
+
+
+cd paymentorder
+
+mkdir images
+
+
+REM Docker save 
+cd images
+
+docker image save dev.local/temenos/ms-paymentorder-service:DEV > ms-paymentorder-serviceDEV.tar
+
+docker image save dev.local/temenos/ms-paymentorder-ingester:DEV > ms-paymentorder-ingesterDEV.tar
+
+docker image save dev.local/temenos/ms-paymentorder-inboxoutbox:DEV > ms-paymentorder-inboxoutboxDEV.tar
+
+docker image save dev.local/temenos/ms-paymentorder-scheduler:DEV > ms-paymentorder-schedulerDEV.tar
+
+docker image save dev.local/temenos/ms-paymentorder-mongoscripts:DEV > ms-paymentorder-mongoscriptsDEV.tar
+
+
+cd ../../
+
+
+rem Pack the images as a zip
+jar -cMf ../paymentorder-helm-mongo-pack.zip paymentorder/
---
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/paymentorder/start-paymentorder-mongo.bat
@@ -0,0 +1,19 @@
+@echo off
+REM --------------------------------------------------------------
+REM - Script to start paymentorder Service
+REM --------------------------------------------------------------
+
+cd helm-chart
+
+kubectl create namespace mongopaymentorder
+
+helm install dbinit ./dbinit -n mongopaymentorder
+
+timeout /t 90 >nul
+
+kubectl create namespace paymentorder
+
+helm install paymentorder ./svc -n paymentorder --set env.database.DATABASE_KEY=mongodb --set env.database.MONGODB_DBNAME=ms_paymentorder --set env.database.POSTGRESQL_CONNECTIONURL=jdbc:postgresql://ent-postgresqldb-service.postgresql.svc.cluster.local:5432/ms_paymentorder 
+
+
+cd ../
