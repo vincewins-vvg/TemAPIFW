d4124c087429f4c5b1c2e8fadfe9910896dc7199
Mohana Badri Kumaragurubaran <k.mohanabadri@temenos.com>
2021-07-16 21:12:51 +0530

    refs #MFW-809 Db connection exception in HELM package - connection URL changes

--- msfwk-testsql/ms-payments-package/ms-payments-package-docker/src/docker/payments/start-payments-mssql.sh
+++ msfwk-testsql/ms-payments-package/ms-payments-package-docker/src/docker/payments/start-payments-mssql.sh
@@ -21,7 +21,7 @@ kubectl create namespace payments
 cd helm-chart
 
 
-helm install dbinit ./dbinit -n dbinitpayments --set env.sqlinit.databaseKey=sql --set env.sqlinit.databaseName=payments --set env.sqlinit.dbdialect=org.hibernate.dialect.SQLServer2012Dialect --set env.sqlinit.dbusername=sa --set env.sqlinit.dbpassword=Rootroot@12345 --set env.sqlinit.dbconnectionurl=jdbc:sqlserver://<IP>:30015
+helm install dbinit ./dbinit -n dbinitpayments --set env.sqlinit.databaseKey=sql --set env.sqlinit.databaseName=payments --set env.sqlinit.dbdialect=org.hibernate.dialect.SQLServer2012Dialect --set env.sqlinit.dbusername=sa --set env.sqlinit.dbpassword=Rootroot@12345 --set env.sqlinit.dbconnectionurl=jdbc:sqlserver://paymentorder-db-service.payments.svc.cluster.local:1433
 
 
 helm install payments ./svc -n payments --set env.database.host=paymentorder-db-service --set env.database.db_username=sa --set env.database.db_password=Rootroot@12345 --set env.database.database_key=sql  --set env.database.database_name=payments --set env.database.driver_name=com.microsoft.sqlserver.jdbc.SQLServerDriver --set env.database.dialect=org.hibernate.dialect.SQLServer2012Dialect --set env.database.db_connection_url=jdbc:sqlserver://paymentorder-db-service:1433;databaseName=payments --set pit.JWT_TOKEN_ISSUER=$JWT_TOKEN_ISSUER --set pit.JWT_TOKEN_PRINCIPAL_CLAIM=$JWT_TOKEN_PRINCIPAL_CLAIM --set pit.ID_TOKEN_SIGNED=$ID_TOKEN_SIGNED --set pit.JWT_TOKEN_PUBLIC_KEY_CERT_ENCODED=$JWT_TOKEN_PUBLIC_KEY_CERT_ENCODED --set pit.JWT_TOKEN_PUBLIC_KEY=$JWT_TOKEN_PUBLIC_KEY 
