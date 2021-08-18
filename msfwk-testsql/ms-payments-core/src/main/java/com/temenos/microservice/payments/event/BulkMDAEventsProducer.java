ded9a97e843aa9958f189654cb9b0e4151a91c33
Alaguvigneshwar Nagarajan <n.alaguvigneshwar@temenos.com>
2021-08-18 09:22:59 +0530

    MFW-844 sql changes

---
+++ msfwk-testsql/ms-payments-core/src/main/java/com/temenos/microservice/payments/event/BulkMDAEventsProducer.java
@@ -0,0 +1,85 @@
+package com.temenos.microservice.payments.event;
+
+import static com.temenos.microservice.framework.core.logger.constants.LoggerConstants.SCHEDULER_DIAGNOSTIC;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.temenos.microservice.framework.core.FunctionException;
+import com.temenos.microservice.framework.core.bulk.BulkEntities;
+import com.temenos.microservice.framework.core.bulk.BulkEventProducer;
+import com.temenos.microservice.framework.core.data.Entity;
+import com.temenos.microservice.framework.core.data.Operator;
+import com.temenos.microservice.framework.core.function.InvalidInputException;
+import com.temenos.microservice.framework.core.mdal.MdaEvent;
+import com.temenos.microservice.framework.core.mdal.MdalKeys;
+import com.temenos.microservice.framework.core.mdal.ParametersEvent;
+import com.temenos.microservice.framework.core.security.Criteria;
+import com.temenos.microservice.framework.core.security.CriterionImpl;
+import com.temenos.microservice.party.entity.Addresses;
+import com.temenos.microservice.partyv4.util.DaoUtils;
+import com.temenos.microservice.payments.entity.PaymentOrder;
+
+/* 
+ * BulkMDALEventsProducer prepares MDAL Events for all the entities provided. 
+ * BulkMDALEventsProducer raised events are used to synchronize data in MDALCACHE
+ * */
+public class BulkMDAEventsProducer implements BulkEventProducer {
+
+	public MdaEvent constructEvents(BulkEntities bulkEntities) {
+
+		if (bulkEntities == null) {
+
+			return null;
+		}
+		ArrayList<MdalKeys> events = new ArrayList<MdalKeys>();
+
+		SCHEDULER_DIAGNOSTIC.prepareInfo("BulkEntities  Size ::::  " + bulkEntities.getEntities().size())
+				.tag("ClassName", "EntityExtractFunction ").log();
+
+		// For each entity , prepare mdaleventpayload and add it to the MDAL Event
+		bulkEntities.getEntities().forEach((entityObj) -> {
+			// prepare MDAL event based on the entity type
+			try {
+				prepareMDALEvent(bulkEntities.getEntityName(), entityObj, events);
+			} catch (FunctionException e) {
+				SCHEDULER_DIAGNOSTIC.prepareInfo("Exception occurred!  " + e.getMessage())
+						.tag("ClassName", "EntityExtractFunction ").log();
+			}
+
+		});
+
+		MdaEvent mdalevent = new MdaEvent();
+		mdalevent.setMdalevent(events);
+		SCHEDULER_DIAGNOSTIC.prepareInfo("MdalEvent Event Size ::::  " + events.size())
+				.tag("ClassName", "EntityExtractFunction ").log();
+
+		// return mdal event // constant
+		// processBulkMdalEvent
+		return mdalevent;
+	}
+
+	private void prepareMDALEvent(String entityName, Entity entityObj, ArrayList<MdalKeys> events)
+			throws FunctionException {
+		switch (entityName) {
+		case "paymentorder":
+			prepareAndAddMDALEvent(entityObj, events);
+			break;
+		}
+	}
+
+	private void prepareAndAddMDALEvent(Entity entityObj, ArrayList<MdalKeys> events) throws FunctionException {
+		PaymentOrder payment = (PaymentOrder) entityObj;
+		MdalKeys event = new MdalKeys();
+		event.setTarget("MDLPAT.Paymentorder");
+		event.setScope("paymentorder");
+
+		ArrayList<ParametersEvent> parametersList = new ArrayList<>();
+		ParametersEvent parameters = new ParametersEvent();
+		parameters.setKey("paymentOrderId");
+		parameters.setValue(payment.getPaymentOrderId());
+		parametersList.add(parameters);
+		event.setParameters(parametersList);
+		events.add(event);
+	}
+}
