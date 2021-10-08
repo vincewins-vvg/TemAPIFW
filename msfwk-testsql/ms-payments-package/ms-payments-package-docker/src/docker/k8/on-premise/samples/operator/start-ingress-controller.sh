8db205ade247ef3eee255c4a91066fe333a60473
Mohammed Khalid <khalidmd@temenos.com>
2021-10-08 14:42:52 +0530

    MFW-1172 Ingress support in paymentodre-msfwk-testsql

---
+++ msfwk-testsql/ms-payments-package/ms-payments-package-docker/src/docker/k8/on-premise/samples/operator/start-ingress-controller.sh
@@ -0,0 +1,13 @@
+#@echo off
+#REM --------------------------------------------------------------
+#REM - Script to start NGINX Ingress Controller
+#REM --------------------------------------------------------------
+
+export ingressController=$(kubectl get namespace -o jsonpath="{.items[?(@.metadata.name=='ingress-nginx')]true}")
+
+if [ "$ingressController" != "true" ]; then
+	kubectl apply -f nginx-ingress-controller.yaml
+	sleep 30
+fi
+
+#ENDOF
