b057f4eb724b8f632e73781e4044455d3cfe04e9
Abhinaya Ravichandran <r.abhinaya@temenos.com>
2022-06-02 12:25:09 +0530

    Added dbinit helm install

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/on-premise/start-paymentorder-keycloak.sh
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/on-premise/start-paymentorder-keycloak.sh
@@ -12,6 +12,12 @@ export ID_TOKEN_SIGNED=true
 export JWT_TOKEN_PUBLIC_KEY_CERT_ENCODED="MIIClTCCAX0CBgF+041vlTANBgkqhkiG9w0BAQsFADAOMQwwCgYDVQQDDANtc2YwHhcNMjIwMjA3MDkzNzQ4WhcNMzIwMjA3MDkzOTI4WjAOMQwwCgYDVQQDDANtc2YwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCCjlPW4lBYzOF05r5NlXaUSuL9KH23gNUFDISZDgmowdi8EkapXT822UDD6OoG83Y6ql+pZa1BjuNV8p64AMs64hnCXmwAuL1H/xbBTbLwvhVwBvfWAxSvuZ3sJbAYANo0T21pLuiknH2d9egPiYxil9xGbhH1IxVXZpUwvEVE2awvIiBCZit1OwlvXGMzf9Vj0hemI7hdH7xZHa3OWHHUle3ncrA0OsuUhxsdvb+P6xoKZHbhNkD+4MmJrZToAefr+TzfP9Y77j82DYaik7UHEVRdg0MXNr9oRSZ5RU53jOwSJHQgdRuxFgZM/Mj4IE/lNnyt+UevLgViNUx3SjO5AgMBAAEwDQYJKoZIhvcNAQELBQADggEBADpNxerAa4Q8gfo7gMINJLhYGrDwKD2YWuB4NfRyEXbcwJOxGGn4ViNc0yCJEHI0J0pbcAcEvYTYTdCpZVMqJ6bX8bm/bWBDuwlDemLyzV66rw/SEl4aML2yc06bGWn1+v93YHz/lLVLIK8JYPUhYWo+T9DkAZ45cP9wCIu/6/krvj91fcQMlGt8ZVRgbCzZLjq9Zrv34jTXStQU9+Qrbrq4zTfk+grVtC6iWKVRaSXC/2sIp6dTdmf+4VdmMrcHBdfh4xHAtTeiGk2AU7D7AzDJ0R1PLe8K0O7zae418Vfoa5MWZOgRMW3nyX5qCXmhemS8qLMN2B5Mks8yIM9I7aU="
 export JWT_TOKEN_PUBLIC_KEY=""
 
+export MONGODB_CONNECTIONSTR="mongodb+srv://user01:user01@mongodb01.jx2tl.mongodb.net/test"
+
+# IMAGE PROPERTIES
+export tag="DEV"
+export dbinitImage=dev.local/temenos/ms-paymentorder-dbscripts
+export dbinitImagePullSecret=""
 
 
 
@@ -28,6 +34,10 @@ cd k8/on-premise/db
 # call start-mongo-operator.bat
 
 cd ../
+
+kubectl create namespace mongopaymentorder
+
+helm install dbinit ./dbinit -n mongopaymentorder --set image.mongoinit.repository=$dbinitImage --set env.mongoinit.migration=../migration --set imagePullSecrets=$dbinitImagePullSecret --set image.tag=$tag --set env.mongoinit.dbConnectionUrl=\"${MONGODB_CONNECTIONSTR}\"
 
 kubectl create namespace paymentorder
 
