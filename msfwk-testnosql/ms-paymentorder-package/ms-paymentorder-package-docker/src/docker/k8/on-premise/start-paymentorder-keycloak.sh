be5bd43a8f474081a4bf5e5d6d4dd290fbaa8345
Logashwari Subramaniyan <s.logashwari@temenos.com>
2022-06-23 12:04:28 +0530

    PIT Issue Fix

--- msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/on-premise/start-paymentorder-keycloak.sh
+++ msfwk-testnosql/ms-paymentorder-package/ms-paymentorder-package-docker/src/docker/k8/on-premise/start-paymentorder-keycloak.sh
@@ -49,6 +49,10 @@ export database_Name=ms_paymentorder
 
 export db_Connection_Url="mongodb+srv://user01:user01@mongodb01.jx2tl.mongodb.net/test"
 
+# Name             : eventDirectDelivery
+# Description      : If the value is true. Framework will directly deliver the events to respective topics. It skip the <msf>-outbox topic. If the value is false. It will delivers the events to <msf>-outbox topic and event delivery service will delivers the events to respective topic.
+export eventDirectDelivery=\"true\"
+
 # -------------------------------------------------------------
 # 
 # IMAGE PROPERTIES
@@ -59,7 +63,7 @@ export db_Connection_Url="mongodb+srv://user01:user01@mongodb01.jx2tl.mongodb.ne
 export tag="DEV"
 # Name			: dbinitImage
 # Description 	: Specifies the name of Images for dbinitImage that are  pushed to external repositories,
-REM Example			: Consider our external repository is "acr.azurecr.io" and tag is "DEV". It should be like acr.azurecr.io/temenos/ms-paymentorder-service:DEV
+#REM Example			: Consider our external repository is "acr.azurecr.io" and tag is "DEV". It should be like acr.azurecr.io/temenos/ms-paymentorder-service:DEV
 export dbinitImage=dev.local/temenos/ms-paymentorder-dbscripts
 # Name			: dbinit_Image_Pull_Secret
 # Description		: Docker registry secret contains the Oracle Cloud Infrastructure credentials to use when pulling the image. You have to specify the image to pull from Container Registry, including the repository location and the Docker registry secret to use, in the application's manifest file. To build docker registry secret,kindly use kubectl create secret docker-registry <secret-name> --docker-server=<region-key>.ocir.io --docker-username='<tenancy-namespace>/<oci-username>' --docker-password='<oci-auth-token>' --docker-email='<email-address>'. Refer https://docs.oracle.com/en-us/iaas/Content/Registry/Tasks/registrypullingimagesfromocir.htm#:~:text=To%20create%20a%20Docker%20registry%20secret%3A. adapter_Image_Pull_Secret and dbinit_Image_Pull_Secret Specifies the <secret-name> is a name of your choice, that you will use in the manifest file to refer to the secret.
@@ -89,7 +93,7 @@ helm install dbinit ./dbinit -n mongopaymentorder --set image.mongoinit.reposito
 
 kubectl create namespace paymentorder
 
-helm install ponosql ./svc -n paymentorder --set env.database.MONGODB_CONNECTIONSTR=$db_Connection_Url --set env.database.MONGODB_DBNAME=$database_Name --set env.database.POSTGRESQL_CONNECTIONURL=jdbc:postgresql://po-postgresqldb-service.postgresql.svc.cluster.local:5432/paymentorderdb --set env.database.DATABASE_KEY=mongodb --set pit.JWT_TOKEN_ISSUER=$Jwt_Token_Issuer --set pit.JWT_TOKEN_PRINCIPAL_CLAIM=$Jwt_Token_Principal_Claim --set pit.ID_TOKEN_SIGNED=$Id_Token_Signed --set pit.JWT_TOKEN_PUBLIC_KEY_CERT_ENCODED=$Jwt_Token_Public_Key_Cert_Encoded --set pit.JWT_TOKEN_PUBLIC_KEY=$Jwt_Token_Public_Key
+helm install ponosql ./svc -n paymentorder --set env.database.MONGODB_CONNECTIONSTR=$db_Connection_Url --set env.database.MONGODB_DBNAME=$database_Name --set env.database.POSTGRESQL_CONNECTIONURL=jdbc:postgresql://po-postgresqldb-service.postgresql.svc.cluster.local:5432/paymentorderdb --set env.database.DATABASE_KEY=mongodb --set pit.JWT_TOKEN_ISSUER=$Jwt_Token_Issuer --set pit.JWT_TOKEN_PRINCIPAL_CLAIM=$Jwt_Token_Principal_Claim --set pit.ID_TOKEN_SIGNED=$Id_Token_Signed --set pit.JWT_TOKEN_PUBLIC_KEY_CERT_ENCODED=$Jwt_Token_Public_Key_Cert_Encoded --set pit.JWT_TOKEN_PUBLIC_KEY=$Jwt_Token_Public_Key --set env.eventdelivery.outboxdirectdeliveryenabled=$eventDirectDelivery
 
 
 
